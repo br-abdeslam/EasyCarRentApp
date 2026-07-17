57dc951c4fa7073a7fe2e35d4aa9c4cb5305e107
Abdeslam Bourzine <abdeslambourzine@gmail.com>
2026-07-17 10:14:54 +0200

    Add Rental persistence model

---
+++ backend/src/main/java/be/condorcet/easycarrent/entity/RentalStatus.java
@@ -0,0 +1,11 @@
+package be.condorcet.easycarrent.entity;
+
+/**
+ * Lifecycle states a rental can be in.
+ */
+public enum RentalStatus {
+    PLANNED,
+    ACTIVE,
+    COMPLETED,
+    CANCELLED
+}
